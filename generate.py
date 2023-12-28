774b3726706d2e6d77a139d1d5a31f873868eb83
Lukas <lukaskrepel@gmail.com>
2023-12-28 10:36:11 +0100

    Added 2x to road filenames

--- generate.py
+++ generate.py
@@ -153,9 +153,9 @@ def make_infra_overlay_sprites(ground, infra):
     ]
 
 
-road_town_png = lib.AseImageFile('sprites/infrastructure/road_town_overlayalpha.ase')
+road_town_png = lib.AseImageFile('sprites/infrastructure/road_town_overlayalpha_2x.ase')
 road_town = tmpl_roadtiles(road_town_png, 0, 0, 2)
-road_png = lib.AseImageFile('sprites/infrastructure/road_overlayalpha.ase')
+road_png = lib.AseImageFile('sprites/infrastructure/road_overlayalpha_2x.ase')
 road = tmpl_roadtiles(road_png, 0, 0, 2)
 replace_old(1313, make_infra_overlay_sprites(general_concrete, road_town))
 replace_old(1332, make_infra_overlay_sprites(temperate_ground_2x, road))
