bd7ef51eda3614a653a2c74f7c36897271dbda1c
dP <dp@dpointer.org>
2024-01-02 17:34:48 +0530

    Add newgrf parameter to switch between thin and thick sprite versions

--- generate.py
+++ generate.py
@@ -16,7 +16,9 @@ TERRAIN_DIR = SPRITE_DIR / 'terrain'
 VEHICLE_DIR = SPRITE_DIR / 'vehicles'
 INFRA_DIR = SPRITE_DIR / 'infrastructure'
 STATION_DIR = SPRITE_DIR / 'stations'
-MODES = (ALL_CLIMATES, TEMPERATE, ARCTIC, TOYLAND, TROPICAL)
+THICK = 1 << 4
+THIN = 2 << 4
+MODES = (TEMPERATE, ARCTIC, TOYLAND, TROPICAL)
 
 g = grf.NewGRF(
     grfid=b'TODO',
@@ -25,13 +27,17 @@ g = grf.NewGRF(
     min_compatible_version=0,
     version=0,
 )
+g.add_bool_parameter(
+    name='Use thin lines',
+    description='Changes between graphical styles for 2x zoom',
+    default=False,
+)
 
 old_sprites = defaultdict(dict)
 new_sprites = defaultdict(lambda: defaultdict(dict))
 
 
-def replace_old(first_id, sprites, *, mode=ALL_CLIMATES):
-    assert mode in MODES
+def replace_old(first_id, sprites, *, mode=0):
     if isinstance(sprites, (grf.Resource, grf.ResourceAction)):
         sprites = [sprites]
 
@@ -41,8 +47,7 @@ def replace_old(first_id, sprites, *, mode=ALL_CLIMATES):
         old_sprites[mode][first_id + i] = s
 
 
-def replace_new(set_type, offset, sprites, *, mode=ALL_CLIMATES):
-    assert mode in MODES
+def replace_new(set_type, offset, sprites, *, mode=0):
     if isinstance(sprites, (grf.Resource, grf.ResourceAction)):
         sprites = [sprites]
     for i, s in enumerate(sprites):
@@ -94,12 +99,19 @@ def tmpl_groundtiles_extra(name, img, zoom):
 # Normal land
 ase_temperate_ground_1x = lib.AseImageFile(TERRAIN_DIR / 'temperate_groundtiles_1x.ase', colourkey=(0, 0, 255))
 ase_temperate_ground_2x = lib.AseImageFile(TERRAIN_DIR / 'temperate_groundtiles_2x.ase', colourkey=(0, 0, 255))
+ase_temperate_ground_2x_thin = lib.AseImageFile(TERRAIN_DIR / 'temperate_groundtiles_2x_thin.ase', colourkey=(0, 0, 255))
 make_ground = lambda name, y: tmpl_groundtiles(name, ase_temperate_ground_1x, ase_temperate_ground_2x, 0)
+make_ground_thin = lambda name, y: tmpl_groundtiles(name, ase_temperate_ground_1x, ase_temperate_ground_2x_thin, 0)
 temperate_ground = make_ground('temperate_ground', 0)
-replace_old(3924, make_ground('temperate_ground_bare', 144))  # 0% grass
-replace_old(3943, make_ground('temperate_ground_33', 96))   # 33% grass
-replace_old(3962, make_ground('temperate_ground_66', 48))   # 66% grass
-replace_old(3981, temperate_ground)  # 100% grass
+temperate_ground_thin = make_ground_thin('temperate_ground', 0)
+replace_old(3924, make_ground('temperate_ground_bare', 144), mode=THICK)  # 0% grass
+replace_old(3943, make_ground('temperate_ground_33', 96), mode=THICK)   # 33% grass
+replace_old(3962, make_ground('temperate_ground_66', 48), mode=THICK)   # 66% grass
+replace_old(3981, temperate_ground, mode=THICK)  # 100% grass
+replace_old(3924, make_ground_thin('temperate_ground_bare', 144), mode=THIN)  # 0% grass
+replace_old(3943, make_ground_thin('temperate_ground_33', 96), mode=THIN)   # 33% grass
+replace_old(3962, make_ground_thin('temperate_ground_66', 48), mode=THIN)   # 66% grass
+replace_old(3981, temperate_ground_thin, mode=THIN)  # 100% grass
 
 ase = lib.AseImageFile(TERRAIN_DIR / 'temperate_groundtiles_rough_2x.ase', colourkey=(0, 0, 255))
 replace_old(4000, tmpl_groundtiles_extra('temperate_rough', ase, ZOOM_2X))
@@ -119,27 +131,28 @@ general_concrete = tmpl_groundtiles('general_concrete', ase1x, ase2x, 0)
 replace_old(1420, general_concrete[0])
 
 
-def on_grass(sprite):
-    grass = temperate_ground[0].get_sprite(zoom=sprite.zoom)
+def on_grass(sprite, thin):
+    ground = temperate_ground_thin[0] if thin else temperate_ground[0]
+    grass = ground.get_sprite(zoom=sprite.zoom)
     return lib.CompositeSprite((grass, sprite))
 
 
 @lib.template(grf.FileSprite)
-def tmpl_airport_tiles(funcs, z):
+def tmpl_airport_tiles(funcs, z, thin):
     func, order = funcs
     with_light = lambda *args, **kw: lib.MagentaToLight(func(*args, **kw), order(*args, **kw))
     return [
         func('apron', z * (1 + 0), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0),
         func('stand', z * (1 + 65), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0),
-        on_grass(func('taxi_ns_west', z * (1 + 455), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0)),
-        on_grass(func('taxi_ew_south', z * (1 + 520), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0)),
-        on_grass(func('taxi_xing_south', z * (1 + 585), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0)),
-        on_grass(func('taxi_xing_west', z * (1 + 650), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0)),
-        on_grass(func('taxi_ns_ctr', z * (1 + 715), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0)),
-        on_grass(func('taxi_xing_east', z * (1 + 780), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0)),
-        on_grass(func('taxi_ns_east', z * (1 + 845), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0)),
-        on_grass(func('taxi_ew_north', z * (1 + 910), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0)),
-        on_grass(func('taxi_ew_ctr', z * (1 + 975), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0)),
+        on_grass(func('taxi_ns_west', z * (1 + 455), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0), thin),
+        on_grass(func('taxi_ew_south', z * (1 + 520), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0), thin),
+        on_grass(func('taxi_xing_south', z * (1 + 585), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0), thin),
+        on_grass(func('taxi_xing_west', z * (1 + 650), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0), thin),
+        on_grass(func('taxi_ns_ctr', z * (1 + 715), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0), thin),
+        on_grass(func('taxi_xing_east', z * (1 + 780), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0), thin),
+        on_grass(func('taxi_ns_east', z * (1 + 845), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0), thin),
+        on_grass(func('taxi_ew_north', z * (1 + 910), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0), thin),
+        on_grass(func('taxi_ew_ctr', z * (1 + 975), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0), thin),
         with_light('runway_a', z * (1 + 130), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0),
         with_light('runway_b', z * (1 + 195), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0),
         with_light('runway_c', z * (1 + 260), z, 64 * z, 32 * z - 1, xofs=-31 * z, yofs=0),
@@ -159,10 +172,14 @@ ase1x = lib.aseidx(INFRA_DIR / 'airport_modern_1x.ase', ignore_layer='Light orde
 ase1x_order = lib.aseidx(INFRA_DIR / 'airport_modern_1x.ase', layer='Light order')
 ase2x = lib.aseidx(INFRA_DIR / 'airport_modern_2x.ase', ignore_layer='Light order')
 ase2x_order = lib.aseidx(INFRA_DIR / 'airport_modern_2x.ase', layer='Light order')
-airport_tiles = tmpl_airport_tiles('airport_modern', (ase1x, ase1x_order), (ase2x, ase2x_order))
-replace_old(2634, airport_tiles[:16])
-replace_new(0x15, 86, airport_tiles[16])
-replace_new(0x10, 12, airport_tiles[17])
+airport_tiles = tmpl_airport_tiles('airport_modern', (ase1x, ase1x_order), (ase2x, ase2x_order), False)
+replace_old(2634, airport_tiles[:16], mode=THICK)
+replace_new(0x15, 86, airport_tiles[16], mode=THICK)
+replace_new(0x10, 12, airport_tiles[17], mode=THICK)
+airport_tiles = tmpl_airport_tiles('airport_modern', (ase1x, ase1x_order), (ase2x, ase2x_order), True)
+replace_old(2634, airport_tiles[:16], mode=THIN)
+replace_new(0x15, 86, airport_tiles[16], mode=THIN)
+replace_new(0x10, 12, airport_tiles[17], mode=THIN)
 
 
 @lib.template(grf.FileSprite)
@@ -233,10 +250,13 @@ def make_infra_overlay_sprites(ground, infra):
 
 road_town = tmpl_roadtiles('road_town', INFRA_DIR / 'road_town_1x.ase', INFRA_DIR / 'road_town_2x.ase', 0, 0)
 road = tmpl_roadtiles('road', INFRA_DIR / 'road_1x.ase', INFRA_DIR / 'road_2x.ase', 0, 0)
+road_thin = tmpl_roadtiles('road', INFRA_DIR / 'road_1x.ase', INFRA_DIR / 'road_2x_thin.ase', 0, 0)
 replace_old(1313, make_infra_overlay_sprites(general_concrete, road_town))
-replace_old(1332, make_infra_overlay_sprites(temperate_ground, road), mode=TEMPERATE)
+replace_old(1332, make_infra_overlay_sprites(temperate_ground, road), mode=TEMPERATE | THICK)
+replace_old(1332, make_infra_overlay_sprites(temperate_ground_thin, road_thin), mode=TEMPERATE | THIN)
 road_noline = tmpl_roadtiles('road_noline', INFRA_DIR / 'road_noline_1x.ase', INFRA_DIR / 'road_noline_2x.ase', 0, 0)
-replace_old(1332, make_infra_overlay_sprites(temperate_ground, road_noline), mode=TROPICAL)
+replace_old(1332, make_infra_overlay_sprites(temperate_ground, road_noline), mode=TROPICAL | THICK)
+replace_old(1332, make_infra_overlay_sprites(temperate_ground_thin, road_noline), mode=TROPICAL | THIN)
 replace_old(1351, make_infra_overlay_sprites(tropical_desert, road_noline))
 
 
@@ -315,7 +335,10 @@ imgfront1x = lib.aseidx(STATION_DIR / 'roaddepots_1x.ase', ignore_layer='Behind'
 imgback1x = lib.aseidx(STATION_DIR / 'roaddepots_1x.ase', layer='Behind')
 imgfront2x = lib.aseidx(STATION_DIR / 'roaddepots_2x.ase', ignore_layer='Behind')
 imgback2x = lib.aseidx(STATION_DIR / 'roaddepots_2x.ase', layer='Behind')
-replace_old(1408, tmpl_road_depot('road_depot', (imgfront1x, imgback1x), (imgfront2x, imgback2x)))
+imgfront2x_thin = lib.aseidx(STATION_DIR / 'roaddepots_2x_thin.ase', ignore_layer='Behind')
+imgback2x_thin = lib.aseidx(STATION_DIR / 'roaddepots_2x_thin.ase', layer='Behind')
+replace_old(1408, tmpl_road_depot('road_depot', (imgfront1x, imgback1x), (imgfront2x, imgback2x)), mode=THICK)
+replace_old(1408, tmpl_road_depot('road_depot', (imgfront1x, imgback1x), (imgfront2x_thin, imgback2x_thin)), mode=THIN)
 
 
 @lib.template(grf.FileSprite)
@@ -362,7 +385,8 @@ def make_shore_sprites(ground, water):
     ORDER = [16, 1, 2, 3, 4, 17, 6, 7, 8, 9, 15, 11, 12, 13, 14, 18]
     return [compose_sprites(ground[i], water[i]) for i in ORDER]
 
-replace_new(0x0d, 0, make_shore_sprites(temperate_ground, water))
+replace_new(0x0d, 0, make_shore_sprites(temperate_ground, water), mode=THICK)
+replace_new(0x0d, 0, make_shore_sprites(temperate_ground_thin, water), mode=THIN)
 
 
 def group_ranges(sprites):
@@ -381,14 +405,23 @@ def group_ranges(sprites):
         yield (last_id - len(cur_range) + 1, cur_range)
 
 
-for mode in MODES:
+for mode in set(old_sprites.keys()) | set(new_sprites.keys()):
     ranges = list(group_ranges(old_sprites[mode]))
-    if not ranges and not new_sprites[mode]:
+    if not ranges and mode not in new_sprites:
         continue
 
-    if mode != ALL_CLIMATES:
-        if_value = {TEMPERATE: 0, ARCTIC: 1, TROPICAL: 2, TOYLAND: 3}[mode]
-        g.add(grf.If(is_static=False, variable=0x83, condition=0x03, value=if_value, skip=0, varsize=1))
+    climate_value = thin_value = None
+    if mode & 0xf0 == THICK:
+        thin_value = 0
+    if mode & 0xf0 == THIN:
+        thin_value = 1
+    if mode & 15 > 0:
+        climate_value = {TEMPERATE: 0, ARCTIC: 1, TROPICAL: 2, TOYLAND: 3}[mode & 15]
+
+    if climate_value is not None:
+        g.add(grf.If(is_static=False, variable=0x83, condition=0x03, value=climate_value, skip=0, varsize=1))  # skip if climate != value
+    if thin_value is not None:
+        g.add(grf.If(is_static=False, variable=0x00, condition=0x03, value=thin_value, skip=0, varsize=1))  # skip if thin param != value
 
     if ranges:
         g.add(grf.ReplaceOldSprites([(offset, len(sprites)) for offset, sprites in ranges]))
@@ -399,7 +432,7 @@ for mode in MODES:
         for offset, sprites in group_ranges(sprite_dict):
             g.add(grf.ReplaceNewSprites(set_type, len(sprites), offset=offset))
             g.add(*sprites)
-    if mode != ALL_CLIMATES:
+    if climate_value is not None or thin_value is not None:
         g.add(grf.Label(0, b''))
 
 
